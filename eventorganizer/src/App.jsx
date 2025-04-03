9cfb5fcd4c11e284b17a54d8d9d8b13826275419
Vjollca Baxhaku <vjollcebaxhaku@gmail.com>
2025-04-03 03:51:56 +0200

    Install required packages and update code for proper imports

--- eventorganizer/src/App.jsx
+++ eventorganizer/src/App.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap-icons/font/bootstrap-icons.css";
 import Dashboard from "./admin/Dashboard";
 
 function App() {
