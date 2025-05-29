5fdaa4288cc19b110b663d830177c6e5ea80e754
BlertaXheladini <blerta.xheladini03@gmail.com>
2025-05-30 01:47:53 +0200

    adds

--- organizingevents/src/User/include/Footer.js
+++ organizingevents/src/User/include/Footer.js
@@ -1,16 +1,19 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./Footer.css";
 
 const Footer = () => {
   return (
     <footer className="footer-container">
       <div className="footer-links">
-        <a href="#">Home</a>
-        <a href="#">About Us</a>
-        <a href="#">Services</a>
-        <a href="#">Events</a>
-        <a href="#">Restaurants</a>
-        <a href="#">Contact</a>
+        <Link to="/">Home</Link>
+        <Link to="/aboutus">About Us</Link>
+        <Link to="/restaurantlist">Restaurants</Link>
+        <Link to="/eventlist">Events</Link>
+        <Link to="/stafflist">Staff</Link>
+        <Link to="/contact">Contact</Link>
+        <Link to="/login">Login</Link>
+        <Link to="/register">Register</Link>
       </div>
       <div className="footer-icons">
         <a href="https://www.facebook.com" target="_blank" rel="noreferrer">
